ce81e69601c809438f9205d42006469ce1545cd8
Anubhav <anubhavanandjsr@gmail.com>
2023-06-23 13:18:09 +0530

    Shopping cart

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'MacBook Pro',
+        price: 1500,
+        image: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/24371433/236492_MacBook_Pro_16__2023__AKrales_0141.jpg',
+      },
+      {
+        id: '2',
+        name: 'Watch',
+        price: 250,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYCcuwqWQj3n3QMqnUyk-qbEbN2KNWPwddTw&usqp=CAU',
+      },
+      {
+        id: '3',
+        name: 'Sneakers',
+        price: 100,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSclp5g9V0peyQ-9-6uPbAyUrtj0mgyOm-ZA&usqp=CAU',
+      },
+      {
+        id: '4',
+        name: 'Bag',
+        price: 30,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4MqjcsDHX1IPXNqICCNVeSd6knIJniU7QPA&usqp=CAU',
+      },
+    ],
+  };
+  export default data;
